a8dac9b8bae06062cafc9ae8fc3a97a8e481f1de
Pierre Bleeker <pierrebleeker@154.166.028.145.hva.nl>
2017-03-01 14:30:07 +0100

    Applied joost feedback, working like a charm

--- week-3/opdracht 3 & 4/js/modules/app.js
+++ week-3/opdracht 3 & 4/js/modules/app.js
@@ -3,44 +3,51 @@
 (function () {
 
     // Structures
+    var config = {
+        loaderEl: document.querySelector('.loader'),
+        placeholder:'https://www.cannabisreports.com/images/strains/no_image.png',
+        token: '972ea5f706ca0dc6dbe17db99a834085804ee594',
+        strainsUrl: 'https://www.cannabisreports.com/api/v1.0/strains/',
+        searchUrl: 'https://www.cannabisreports.com/api/v1.0/strains/search/',
+        strainUrl:'https://www.cannabisreports.com/api/v1.0/strains/',
+        strainsWithImage: []
+    }
+    var store = {
+        list:{}
+    }
+
     var app = {
         init: function () {
 
             // Fire routes.init();
             routes.init();
+            filter.init();
         }
     }
-    var routes = {
 
+    var routes = {
         init: function () {
-            // set hash to #user-search
-            // window.location.hash = '#strains';
+            // set hash to #about
+            window.location.hash = '#about';
 
             // call routie
             routie({
                 'about' : function () {
 
-                    // get path name and fire sections.toggle() to show section
-                    var hashName = this.path;
+                    // toggle about section
                     sections.toggle('about');
+
                 },
 
                 'strains' : function () {
-
-                    // get path name and fire sections.toggle() to show section
-                    var hashName = this.path;
-                    request.list('https://www.cannabisreports.com/api/v1.0/strains/');
-
-                    // onclick
-                    filterStrains.onClick();
+                    request.list(config.strainsUrl);
                 },
 
                 'search' : function () {
 
                     // get path name and fire sections.toggle() to show section
                     var hashName = this.path;
-                    //  get strainList data
-                    // strainsList.renderData();
+
                     sections.toggle(hashName);
 
                     var inputButton = document.querySelector('input[type="button"]');
@@ -48,8 +55,8 @@
                     inputButton.addEventListener('click', function() {
 
                         var userInput = document.querySelector('input[type="text"]').value;
-                        console.log(userInput);
-                        request.list('https://www.cannabisreports.com/api/v1.0/strains/search/', userInput, false);
+                        request.list(config.searchUrl, userInput, false);
+
                     });
                 },
 
@@ -60,84 +67,80 @@
                     // get StrainDtail data
                     // strainDetail.renderData(strainId);
 
-                    request.detail('https://www.cannabisreports.com/api/v1.0/strains/', strainId);
+                    request.detail(config.strainUrl, strainId);
                     // sections.toggle('strain-detail');
+
+
                 }
             });
         }
     }
 
-    var filterStrains = {
-        onClick: function(){
-            document.querySelector('.strains-list__image-filter').addEventListener('click', function(){
-                request.list('https://www.cannabisreports.com/api/v1.0/strains/', false, true);
-            })
-        }
+    var filter = {
+        init:function () {
+            var filterButton = document.querySelector('.strains-list__image-filter');
+            filterButton.addEventListener('click', function(){
+                request.list(config.strainsUrl, false, true);
+            });
+        },
 
+        hasImage: function (object) {
+            config.strainsWithImage.push(object);
+            return object.image != config.placeholder;
+        }
     }
 
     var request = {
 
         list: function (url, userInput, filtered) {
+
             // if userInput is undefined (on search page)
-
             if(!userInput && !filtered) {
+
+                // show loader
+                loader.show();
+
                 // call data
                 aja()
-                    .url(url + '?=972ea5f706ca0dc6dbe17db99a834085804ee594')
+                    .url(url + '?=' + config.token)
                     .cache(false)
                     .type('jsonp')
                     .cache('false')
                     .on('success', function (data) {
-                        console.log('!userinput');
                         console.log(data);
-                        renderData.list(data);
+                        // map data.data to data
+
+                        renderData.list(data.data, 'strains');
                         //sections toggle
+                        store.list = data.data;
                     })
 
                     .go();
 
-            } else if (filtered){
-                // call data
-                aja()
-                    .url(url + '?=972ea5f706ca0dc6dbe17db99a834085804ee594')
-                    .cache(false)
-                    .type('jsonp')
-                    .cache('false')
-                    .on('success', function (data) {
-                        var strainsWithImage = [];
-
-                        function hasImage(object) {
-                            console.log(object);
-                            console.log(object.image != 'https://www.cannabisreports.com/images/strains/no_image.png');
-                            strainsWithImage.push(object);
-
-                            return object.image != 'https://www.cannabisreports.com/images/strains/no_image.png';
-                        }
-                        var data = data.data;
-
-                        var filteredData = data.filter(hasImage);
-
-                        var dataObj= { data: filteredData};
-                        console.log(dataObj);
-                        console.log(filteredData);
-
-                        renderData.list(dataObj);
-                    })
-
-                    .go();
             }
+            else if (filtered){
+
+                // show loader
+                loader.show();
+
+                var filteredData = store.list.filter(filter.hasImage);
+                renderData.list(filteredData, 'strains');
+            }
+                
             else {
+                // show loader
+                loader.show();
+
+                document.querySelector('.strains-list').classList.remove('hidden');
+
                 // call data
                 aja()
-                    .url(url + userInput + '?=972ea5f706ca0dc6dbe17db99a834085804ee594')
+                    .url(url + userInput + '?=' + config.token)
                     .cache(false)
                     .type('jsonp')
                     .cache('false')
                     .on('success', function (data) {
-                        console.log('nopthing');
-                        console.log(data);
-                        renderData.list(data);
+                        renderData.list(data.data, 'search');
                         //sections toggle
                     })
 
@@ -146,15 +149,17 @@
         },
 
         detail: function (url, strainName) {
+            // show loader
+            loader.show();
+            
             // call data
             aja()
-                .url(url + strainName +'?=972ea5f706ca0dc6dbe17db99a834085804ee594')
+                .url(url + strainName +'?=' + config.token)
                 .cache(false)
                 .type('jsonp')
                 .cache('false')
                 .on('success', function(data){
-                    console.log(data);
-                    renderData.detail(data);
+                    renderData.detail(data.data);
                     //sections toggle
                 })
 
@@ -163,10 +168,11 @@
     }
 
     var renderData = {
-        list: function (data) {
-
+        list: function (data, parentElement) {
             // declare target parent for transparancy js
-            var strains = document.querySelector('.strains-list');
+
+            // var strains = document.querySelector('.strains-list');
+            var strains = document.querySelector('#' + parentElement + '> .strains-list');
 
             var directives = {
 
@@ -191,11 +197,9 @@
             }
 
             // call Transparency
-            Transparency.render(strains, data.data, directives);
+            Transparency.render(strains, data, directives);
 
             // call sections.toggle() to show activeSection and hide other sections
-            console.log(window.location.hash.substr(1));
-
 
             sections.toggle(window.location.hash.substr(1));
         },
@@ -234,7 +238,7 @@
             };
 
             // call Transparency
-            Transparency.render(strain, data.data, directives);
+            Transparency.render(strain, data, directives);
 
             sections.toggle('strain-detail');
         }
@@ -248,18 +252,33 @@
             var sections = document.querySelectorAll('section');
 
             // sections.classList.add('hidden');
-            console.log(sections);
 
             sections.forEach(function(section) {
-
-                console.log(section);
 
                 section.classList.add('hidden');
             });
 
+            // hide loader
+
+            if(activeSection === 'search' || activeSection === 'about' || activeSection === 'strains' || activeSection === 'strain-detail') {
+                loader.hide();
+
+            } else {
+                loader.show();
+            }
 
             // show active section by removing hidden class
             document.getElementById(activeSection).classList.remove('hidden');
+        }
+    }
+
+    var loader = {
+        show: function () {
+            config.loaderEl.classList.remove('hidden');
+        },
+
+        hide: function () {
+            config.loaderEl.classList.add('hidden');
         }
     }
 
